120546ddac4fafc8ee4617eeeee367c3f28f9c51
Fredrik Nilsson <frednils@student.chalmers.se>
2017-03-09 15:00:30 +0100

    new branch for bluetooth low energy. Currently sends advertisements packets

--- boards/nrf51dk/src/main.rs
+++ boards/nrf51dk/src/main.rs
@@ -114,6 +114,7 @@ pub struct Platform {
     temp: &'static capsules::temp_nrf51dk::Temperature<'static, nrf51::temperature::Temperature>,
     rng: &'static capsules::rng::SimpleRng<'static, nrf51::trng::Trng<'static>>,
     aes: &'static capsules::symmetric_encryption::Crypto<'static, nrf51::aes::AesECB>,
+    radio: &'static capsules::radio_nrf51dk::Radio<'static, nrf51::radio::Radio>,
 }
 
 
@@ -140,6 +141,7 @@ impl kernel::Platform for Platform {
             9 => f(Some(self.button)),
             14 => f(Some(self.rng)),
             17 => f(Some(self.aes)),
+            33 => f(Some(self.radio)),
             36 => f(Some(self.temp)),
             _ => f(None),
         }
@@ -267,6 +269,13 @@ pub unsafe fn reset_handler() {
     nrf51::aes::AESECB.ecb_init();
     nrf51::aes::AESECB.set_client(aes);
 
+    let radio = static_init!(
+     capsules::radio_nrf51dk::Radio<'static, nrf51::radio::Radio>,
+     capsules::radio_nrf51dk::Radio::new(&mut nrf51::radio::RADIO, kernel::Container::create(),
+     &mut capsules::radio_nrf51dk::BUF),
+        160/8);
+    nrf51::radio::RADIO.set_client(radio);
+    radio.capsule_init();
 
     // Start all of the clocks. Low power operation will require a better
     // approach than this.
@@ -288,6 +297,7 @@ pub unsafe fn reset_handler() {
         temp: temp,
         rng: rng,
         aes: aes,
+        radio: radio,
     };
 
     alarm.start();
