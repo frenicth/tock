91d530b05f1e473e8f120c407bd469d56dcf069a
Fredrik Nilsson <frednils@student.chalmers.se>
2017-03-20 15:24:13 +0100

    working on virtual timers, removed some panics...

--- boards/nrf51dk/src/main.rs
+++ boards/nrf51dk/src/main.rs
@@ -112,7 +112,7 @@ pub struct Platform {
     button: &'static capsules::button::Button<'static, nrf51::gpio::GPIOPin>,
     temp: &'static capsules::temp_nrf51dk::Temperature<'static, nrf51::temperature::Temperature>,
     rng: &'static capsules::rng::SimpleRng<'static, nrf51::trng::Trng<'static>>,
-    radio: &'static capsules::radio_nrf51dk::Radio<'static, nrf51::radio::Radio, nrf51::timer::TimerAlarm>,
+    radio: &'static capsules::radio_nrf51dk::Radio<'static, nrf51::radio::Radio, VirtualMuxAlarm<'static, Rtc>>,
 }
 
 
@@ -232,6 +232,13 @@ pub unsafe fn reset_handler() {
                          12);
     virtual_alarm1.set_client(timer);
 
+    let radio_virtual_alarm = static_init!(
+        VirtualMuxAlarm<'static, Rtc>,
+        VirtualMuxAlarm::new(mux_alarm),
+        24);
+
+
+
     let temp = static_init!(
         capsules::temp_nrf51dk::Temperature<'static, nrf51::temperature::Temperature>,
         capsules::temp_nrf51dk::Temperature::new(&mut nrf51::temperature::TEMP,
@@ -245,11 +252,17 @@ pub unsafe fn reset_handler() {
     nrf51::trng::TRNG.set_client(rng);
 
     let radio = static_init!(
-     capsules::radio_nrf51dk::Radio<'static, nrf51::radio::Radio, nrf51::timer::TimerAlarm>,
-     capsules::radio_nrf51dk::Radio::new(&mut nrf51::radio::RADIO, kernel::Container::create(),
-     &mut capsules::radio_nrf51dk::BUF, &mut nrf51::timer::ALARM1),
+     capsules::radio_nrf51dk::Radio<'static, nrf51::radio::Radio, VirtualMuxAlarm<'static, Rtc>>,
+     capsules::radio_nrf51dk::Radio::new(
+         &mut nrf51::radio::RADIO,
+         kernel::Container::create(),
+         &mut capsules::radio_nrf51dk::BUF,
+         radio_virtual_alarm),
         192/8);
     nrf51::radio::RADIO.set_client(radio);
+
+    radio_virtual_alarm.set_client(radio);
+
     radio.capsule_init();
 
     // Start all of the clocks. Low power operation will require a better
