c8a6a52d22d1e814c5956873e2d4bcc0c1403c2d
niklasad1 <niklasadolfsson1@gmail.com>
2017-06-05 14:51:38 +0200

    added dummy app to be used by a phone

---
+++ userland/examples/ble_temperature_sensor/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ble.h>
+#include <timer.h>
+#include <string.h>
+#include "temperature.h"
+
+
+static unsigned char t[1];
+static bool got_temp = false;
+
+static void callback(int temp,
+    __attribute__ ((unused)) int not_used2,
+    __attribute__ ((unused)) int arg2,
+    __attribute__ ((unused)) void *ud) {
+  t[0] = (unsigned char)temp;
+}
+
+int main(void)
+{
+  printf("starting BLE Temperature APP\r\n");
+
+  temperature_init(callback, NULL);
+  temperature_measure();
+  delay_ms(10);
+
+  unsigned char name[] = "TockOS";
+
+  ble_adv_data(BLE_HS_ADV_TYPE_COMP_NAME, sizeof(name) - 1, name);
+  ble_adv_data(BLE_HS_ADV_TYPE_MFG_DATA, 1, t);
+  ble_adv_start();
+
+  for(;;) {
+    got_temp = false;
+    delay_ms(1000);
+    ble_adv_stop();
+    ble_adv_clear_data();
+    temperature_measure();
+    delay_ms(10);
+    ble_adv_data(BLE_HS_ADV_TYPE_COMP_NAME, sizeof(name) - 1, name);
+    ble_adv_data(BLE_HS_ADV_TYPE_MFG_DATA, 1, t);
+    ble_adv_start();
+  }
+
+  return 0;
+}
