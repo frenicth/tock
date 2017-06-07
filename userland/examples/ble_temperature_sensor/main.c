c15b9635fd78d47ed09b6bfa129dc0aa408db4da
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2017-06-07 16:50:39 +0200

    added encryption as well in our demo app

--- userland/examples/ble_temperature_sensor/main.c
+++ userland/examples/ble_temperature_sensor/main.c
@@ -5,16 +5,22 @@
 #include <timer.h>
 #include <string.h>
 #include "temperature.h"
+#include <aes.h>
+
 
 int main(void)
 {
   printf("\rstarting BLE Temperature APP\r\n");
   unsigned char name[] = "TockOS";
   unsigned char buf[5];
-  
-  int err;
+  unsigned char key[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
 
-  for(;;) {
+  unsigned char ctr[] = {0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff};
+
+  int err;
+  aes128_configure_key(key, sizeof(key));
+  
+    for(;;) {
     err = temperature_measure();
     if(err >= 0) {
       buf[0] = (unsigned char)err;
@@ -22,19 +28,21 @@ int main(void)
     else {
       printf("Negative temperature value discard it\r\n");
     }
-    
+
     if(rng_sync(buf+1, 4, 4) < 0) {
       printf("rng error\r\n");
     }
+    
+    aes128_encrypt_ctr(buf, sizeof(buf), ctr, sizeof(ctr));
 
     // start
     ble_adv_data(BLE_HS_ADV_TYPE_COMP_NAME, sizeof(name) - 1, name);
     ble_adv_data(BLE_HS_ADV_TYPE_MFG_DATA, sizeof(buf), buf);
     ble_adv_start();
-    
+
     // sleep
     delay_ms(1000);
-    
+
     // top
     ble_adv_stop();
     ble_adv_clear_data();
--- userland/libtock/aes.c
+++ userland/libtock/aes.c
@@ -1,6 +1,24 @@
 #include "aes.h"
 
-int aes128_init(subscribe_cb callback, void *ud) {
+
+struct aes_data {
+  bool fired;
+  int received;
+};
+
+static struct aes_data result = { .fired = false, .received = 0};
+
+
+static void aes_cb(int cb,
+    __attribute__ ((unused)) int len,
+    __attribute__ ((unused)) int arg2,
+    __attribute__ ((unused)) void *ud) {
+  result.fired = true;
+  result.received = cb;
+}
+
+
+int aes128_set_callback(subscribe_cb callback, void *ud) {
   return subscribe(AES_DRIVER, 0, callback, ud);
 }
 
@@ -13,25 +31,35 @@ int aes128_configure_key(const unsigned char* key, unsigned char len) {
 }
 
 int aes128_encrypt_ctr(unsigned const char* buf, unsigned char buf_len, unsigned const char* ctr, unsigned char ctr_len) {
-  int err = allow(AES_DRIVER, AES_DATA, (void*)buf, buf_len);
-  if (err < 0)  {
-    return err;
-  }
+  int err;
+  
+  err = aes128_set_callback(aes_cb, NULL);
+  if (err < 0) return err;
+
+  err = allow(AES_DRIVER, AES_DATA, (void*)buf, buf_len);
+  if (err < 0) return err;
+  
+  result.fired = false;
   err = allow(AES_DRIVER, AES_CTR, (void*)ctr, ctr_len);
-  if (err < 0) {
-    return err;
-  }
+  if (err < 0) return err;
+  
   return command(AES_DRIVER, AES_ENC, 0);
+  yield_for(&result.fired);
+  result.received;
 }
 
 int aes128_decrypt_ctr(const unsigned char* buf, unsigned char buf_len, const unsigned char* ctr, unsigned char ctr_len) {
-  int err = allow(AES_DRIVER, AES_DATA, (void*)buf, buf_len);
-  if (err < 0)  {
-    return err;
-  }
+  int err = aes_set_callback(aes_cb, NULL);
+  if (err < 0) return err;
+
+  err = allow(AES_DRIVER, AES_DATA, (void*)buf, buf_len);
+  if (err < 0) return err;
+  
   err = allow(AES_DRIVER, AES_CTR, (void*)ctr, ctr_len);
-  if (err < 0) {
-    return err;
-  }
+  if (err < 0) return err;
+  
+  result.fired = false;
   return command(AES_DRIVER, AES_DEC, 0);
+  yield_for(&result.fired);
+  return result.received;
 }
