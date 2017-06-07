745ae7e7bb04485b1b2a5c5856ece72c71193a3e
niklasad1 <niklasadolfsson1@gmail.com>
2017-06-07 11:38:13 +0200

    this version seem  to more reliable for some reason

--- userland/examples/ble_temperature_sensor/main.c
+++ userland/examples/ble_temperature_sensor/main.c
@@ -5,8 +5,8 @@
 #include <string.h>
 #include "temperature.h"
 
+
 static unsigned char t[1];
-static unsigned char name[] = "TockOS";
 
 static void callback(int temp,
     __attribute__ ((unused)) int not_used2,
@@ -20,18 +20,24 @@ int main(void)
   printf("starting BLE Temperature APP\r\n");
 
   temperature_init(callback, NULL);
-  ble_adv_set_interval(10);
+  temperature_measure();
+  delay_ms(10);
+
+  unsigned char name[] = "TockOS";
+
+  ble_adv_data(BLE_HS_ADV_TYPE_COMP_NAME, sizeof(name) - 1, name);
+  ble_adv_data(BLE_HS_ADV_TYPE_MFG_DATA, 1, t);
+  ble_adv_start();
 
   for(;;) {
-    temperature_measure();
-    delay_ms(100);
-    ble_adv_data(BLE_HS_ADV_TYPE_COMP_NAME, sizeof(name) - 1, name);
-    ble_adv_data(BLE_HS_ADV_TYPE_MFG_DATA, 1, t);
-    ble_adv_start();
     delay_ms(1000);
     ble_adv_stop();
     ble_adv_clear_data();
-    delay_ms(100);
+    temperature_measure();
+    delay_ms(10);
+    ble_adv_data(BLE_HS_ADV_TYPE_COMP_NAME, sizeof(name) - 1, name);
+    ble_adv_data(BLE_HS_ADV_TYPE_MFG_DATA, 1, t);
+    ble_adv_start();
   }
 
   return 0;
